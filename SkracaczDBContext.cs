851940d35a1f3faa8fb894e224f3c5cfe63326a9
annuszka <annrybak92@gmail.com>
2018-04-01 21:25:06 +0200

    dodanie klasy SkracaczDBContext

---
+++ SkracaczDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using skracacz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace skracacz
+{
+    public class SkracaczDBContext : DbContext
+    {
+        public SkracaczDBContext(DbContextOptions<SkracaczDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Link> Links { get; set; }
+    }
+}
